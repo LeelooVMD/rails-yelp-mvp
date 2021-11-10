f319cf7912c36bdf17593d108be21a7270eb525e
Lilas Marchand <lilasviolettemarchand@gmail.com>
2021-11-10 14:53:22 +0100

    exo 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+popolare = { name: "Popolare", address: "111 Rue Réaumur 75002", phone_number: "09 67 72 30 91", category: "italian" }
+brigade =  { name: "La Brigade", address: "103 Rue Oberkampf", phone_number: "01 48 06 79 56", category: "french" }
+
+[ popolare, brigade ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
